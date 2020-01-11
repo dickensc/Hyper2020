f5f65d03a79c029fc57eb724621dfa44a4920ac2
Vibin Vijay <vibinvijay@eduroam-169-233-187-127.ucsc.edu>
2020-01-10 16:09:20 -0800

    scripts to run inference experiments.

---
+++ run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Run all the experiments.
+
+INFERENCE_DATASETS='lastfm yelp'
+
+function main() {
+    trap exit SIGINT
+
+    local datasetPaths=''
+    for dataset in $INFERENCE_DATASETS; do
+        datasetPaths="${datasetPaths} datasets/${dataset}"
+    done
+
+    echo "Running inference experiments on datasets: [${INFERENCE_DATASETS}]."
+    ./scripts/run_inference_experiments.sh $datasetPaths
+
+    datasetPaths=''
+    for dataset in $MEMORY_DATASETS; do
+        datasetPaths="${datasetPaths} datasets/${dataset}"
+    done
+}
+
+main "$@"
